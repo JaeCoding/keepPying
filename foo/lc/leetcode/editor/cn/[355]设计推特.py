96a89eb129a9835413cd021d48e2f242b6747ed9
“JaeCoding” <641403035@qq.com>
2020-04-13 23:38:09 +0800

    commit

--- foo/lc/leetcode/editor/cn/[355]设计推特.py
+++ foo/lc/leetcode/editor/cn/[355]设计推特.py
@@ -58,54 +58,57 @@ class Twitter:
         """
         Initialize your data structure here.
         """
-        self.followers = collections.defaultdict(set)
-        self.twitters = collections.defaultdict(list)
-        
+        self.users = dict()  # key:value分别对应用户 userId(int) 和 其关注者(list)
+        self.posts = []  # 发布的帖子，每个元素格式为 [userId, tweetId]
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         """
         Compose a new tweet.
         """
-        if len(self.twitters[userId]) == 10:
-            self.twitters[userId].remove(0)
-        self.twitters[userId].append((tweetId, datetime.datetime.now()))
-
-    def cmp(self, a, b):
-
+        self.posts.append([userId, tweetId])  # 将帖子加入 posts 列表
+        if not self.users.get(userId):  # 同步更新用户列表，如果 userId 在字典中的值不存在，则设为初始值 [],否则不操作
+            self.users[userId] = []
 
     def getNewsFeed(self, userId: int) -> List[int]:
-
         """
         Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
         """
-        heap = []
-        return None
-        
+        if userId not in self.users.keys():  # 检查 userId 的合法性,如果该用户不存在，直接返回
+            return
+        else:  # 用户存在
+            ids = [userId] + self.users.get(userId)  # 计算待排查id，包括用户自身 id 还有他 follow 的人的 id
+            tmp = []  # 待返回的结果集
+            count = 10  # 计数器：排查最新的10条
+            # 对于(每个[userId,tweetId])
+            for post in self.posts[-1:-(len(self.posts) + 1):-1]:  # 开始排查，并将 tweetId 加入结果集
+                if count > 0:
+                    # post[0]
+                    if post[0] in ids:
+                        tmp.append(post[1])
+                        count -= 1
+            return tmp
 
     def follow(self, followerId: int, followeeId: int) -> None:
         """
         Follower follows a followee. If the operation is invalid, it should be a no-op.
         """
-        self.followers[followerId].add(followeeId)
-        
+        if followerId not in self.users.keys():  # 检查 followerId 用户的 id 合法性，如果没在 self.users 中出现过，则设为初始值
+            self.users[followerId] = []
+            self.users[followerId].append(followeeId)
+        else:  # followerId 如果在 self.users 中出现过，则直接将 followeeId 直接加入
+            self.users[followerId].append(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         """
         Follower unfollows a followee. If the operation is invalid, it should be a no-op.
         """
-        self.followers[followerId].remove(followeeId)
-
-
-
-a = collections.defaultdict(list)
-a[1].append(3)
-a[1].append(3)
-print(a[1])
-a = datetime.datetime.now()
-time.sleep(1)
-b = datetime.datetime.now()
-print(a < b)
-
+        if followerId not in self.users.keys():  # 检查合法性，如果 followerId 未曾出现，则直接返回
+            return
+        else:
+            if followeeId in self.users[followerId]:  # 检查被移除的 id 的合法性，如果存在直接删除
+                self.users[followerId].remove(followeeId)
+            else:  # 被移除的 id 不存在，返回
+                return
 
 # Your Twitter object will be instantiated and called as such:
 # obj = Twitter()
@@ -113,4 +116,6 @@ print(a < b)
 # param_2 = obj.getNewsFeed(userId)
 # obj.follow(followerId,followeeId)
 # obj.unfollow(followerId,followeeId)
-# leetcode submit region end(Prohibit modification and deletion)
+
+a = [1,2,3,4]
+print(a[-1:-(len(a) + 1):-1])
